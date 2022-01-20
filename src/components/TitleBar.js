7b665be53f6e75db67e0f65253a436e7c901201e
Ritesh Kumar shukla <riteshshukla2381@gmail.com>
2022-01-21 01:01:02 +0530

    f

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -2,6 +2,6 @@
 
 export function TitleBar(){
     return(
-        <div style={{fontFamily:"Roboto",color:"Black",fontSize:"25px"}}>UnRest API</div>
+        <div style={{fontFamily:"Roboto",color:"Black",fontSize:"25px"}}>Un-Rest API</div>
     )
 }
